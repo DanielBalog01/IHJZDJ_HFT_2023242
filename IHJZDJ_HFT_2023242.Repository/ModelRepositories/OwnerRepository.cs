c3e50010ec974b489b261e9c2ad1cd0c18d72e68
IHJZDJ <IHJZDJ@stud.uni-obuda.hu>
2024-05-22 19:19:30 +0200

    Owner abstract class implenetálása

--- IHJZDJ_HFT_2023242.Repository/ModelRepositories/OwnerRepository.cs
+++ IHJZDJ_HFT_2023242.Repository/ModelRepositories/OwnerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IHJZDJ_HFT_2023242.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace IHJZDJ_HFT_2023242.Repository
 {
-    internal class OwnerRepository
+    public class OwnerRepository : Repository<Owner>, IRepository<Owner>
     {
+        public override Owner Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(Owner item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
