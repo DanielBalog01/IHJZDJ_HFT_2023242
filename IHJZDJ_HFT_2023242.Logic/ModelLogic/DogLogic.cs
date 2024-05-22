2a0dac5cf0da88f98c156fe2395fd336b0cc17cf
IHJZDJ <IHJZDJ@stud.uni-obuda.hu>
2024-05-22 19:29:18 +0200

    GoldenRetDog lekérdezés

--- IHJZDJ_HFT_2023242.Logic/ModelLogic/DogLogic.cs
+++ IHJZDJ_HFT_2023242.Logic/ModelLogic/DogLogic.cs
@@ -48,5 +48,28 @@ namespace IHJZDJ_HFT_2023242.Logic
             if (item.DogName is null || item.DogName.Length < 4) throw new ArgumentException("Wrong player name");
             this.repository.Update(item);
         }
+
+
+        public IEnumerable<Dog> GoldenRetDog()
+        {
+            var result = from source in repository.ReadAll()
+                         from source2 in breedrepository.ReadAll()
+                         where source.BreedId == source2.BreedId && source2.BreedName == "Golden Retriver"
+                         select new Dog
+                         {
+                             DogName = source.DogName,
+                             Age = source.Age,
+                             Breed = source.Breed,
+                             BreedId = source.BreedId,
+                             Owner = source.Owner,
+                             OwnerId = source.OwnerId,
+                             DogId = source.DogId
+                         };
+            return result;
+
+        }
+
+
+
     }
 }
