4199fb8c3b44c1c15e3f66fedd073b9cfd40f696
IHJZDJ <IHJZDJ@stud.uni-obuda.hu>
2024-05-22 19:23:29 +0200

    Owner repo implementálása

--- IHJZDJ_HFT_2023242.Repository/ModelRepositories/OwnerRepository.cs
+++ IHJZDJ_HFT_2023242.Repository/ModelRepositories/OwnerRepository.cs
@@ -9,14 +9,23 @@ namespace IHJZDJ_HFT_2023242.Repository
 {
     public class OwnerRepository : Repository<Owner>, IRepository<Owner>
     {
+        public OwnerRepository(DogDbContext DbCtx) : base(DbCtx)
+        {
+        }
+
         public override Owner Read(int id)
         {
-            throw new NotImplementedException();
+            return DbCtx.Owners.FirstOrDefault(t => t.OwnerId == id);
         }
 
         public override void Update(Owner item)
         {
-            throw new NotImplementedException();
+            var old = Read(item.OwnerId);
+            if (old != null)
+            {
+                old.OwnerName = item.OwnerName;
+                DbCtx.SaveChanges();
+            }
         }
     }
 }
